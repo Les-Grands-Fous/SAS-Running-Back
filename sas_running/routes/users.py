dc293bfb00f9ca841f5e97d0aaee0ca58df05206
Vincent <141185796+Yalcael@users.noreply.github.com>
2023-12-28 04:20:37 +0100

    WIP

--- sas_running/routes/users.py
+++ sas_running/routes/users.py
@@ -1,7 +1,10 @@
+from http import HTTPStatus
+
 from fastapi import APIRouter, Depends
 from sas_running.controllers.users import UserController
 from sas_running.dependencies import get_user_controller
-from sas_running.models.users import UserCreate, UserUpdate
+from sas_running.models.runs import Run
+from sas_running.models.users import UserCreate, UserUpdate, User
 
 router = APIRouter(
     prefix="/users",
@@ -10,19 +13,19 @@ router = APIRouter(
 )
 
 
-@router.get("/")
+@router.get("/", response_model=list[User])
 def get_users(*, user_controller: UserController = Depends(get_user_controller)):
     return user_controller.get_users()
 
 
-@router.get("/{user_id}")
+@router.get("/{user_id}", response_model=User)
 def get_user_by_id(
     *, user_id: int, user_controller: UserController = Depends(get_user_controller)
 ):
     return user_controller.get_user_by_id(user_id)
 
 
-@router.post("/")
+@router.post("/", response_model=User)
 def create_user(
     *,
     user_create: UserCreate,
@@ -31,14 +34,14 @@ def create_user(
     return user_controller.create_user(user_create)
 
 
-@router.delete("/{user_id}")
+@router.delete("/{user_id}", status_code=HTTPStatus.NO_CONTENT)
 def delete_user(
     *, user_id: int, user_controller: UserController = Depends(get_user_controller)
 ):
     user_controller.delete_user(user_id)
 
 
-@router.patch("/{user_id}")
+@router.patch("/{user_id}", response_model=User)
 def update_user(
     *,
     user_id: int,
@@ -48,10 +51,8 @@ def update_user(
     return user_controller.update_user(user_id, user_update)
 
 
-@router.get("/{user_id}/runs")
+@router.get("/{user_id}/runs", response_model=list[Run])
 def get_user_runs(
-    *,
-    user_id: int,
-    user_controller: UserController = Depends(get_user_controller)
+    *, user_id: int, user_controller: UserController = Depends(get_user_controller)
 ):
     return user_controller.get_user_runs(user_id)
--- scripts/import_users.py
+++ scripts/import_users.py
@@ -1,20 +1,22 @@
 import pandas as pd
 import requests
-import json
 
 
-df = pd.read_excel('liste_of_users.xlsx', index_col=None)
+df = pd.read_excel("liste_of_users.xlsx", index_col=None)
 
 
 def create_users(row):
     print("Prénom:", row["Prénom"])
     print("Nom:", row["Nom"])
-    response = requests.post("http://127.0.0.1:5000/users/", json=json.dumps({'first_name': row["Prénom"], 'last_name': row["Nom"]}))
+    url = "http://127.0.0.1:5000/users/"
+    headers = {"content-type": "application/json"}
+    cre_users = {"first_name": row["Prénom"], "last_name": row["Nom"]}
+    response = requests.post(url, json=cre_users, headers=headers)
     print(response.json())
+    print(response.status_code)
     return row
 
 
-print(list(df.columns.values))
 df.apply(create_users, axis=1)
 
-print({'first_name': 'last_name'})
+print({"first_name": "last_name"})
